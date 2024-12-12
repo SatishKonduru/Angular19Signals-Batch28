d999c6534931364617efe5f03a4cbb465beae96c
satish <satishkonduru0225@gmail.com>
2024-12-12 22:13:38 +0530

    ChangeDetection Strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'Angular19Signals';
+  changeDetector = inject(ChangeDetectorRef)
+
+  counter = 0;
+  constructor(){
+    setInterval(() => {
+      this.counter++
+      console.log("Counter: ", this.counter)
+    }, 1000 )
+
+
+    setInterval(() => {
+      this.changeDetector.detectChanges()
+    }, 5000)
+  }
+
+  doNothing(){
+
+  }
+
 }
