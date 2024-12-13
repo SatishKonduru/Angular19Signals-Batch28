329e1dc998aab90304078a38e424b223fa228389
satish <satishkonduru0225@gmail.com>
2024-12-13 22:04:30 +0530

    async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'Angular19Signals';
-  changeDetector = inject(ChangeDetectorRef)
+  // title = 'Angular19Signals';
+  // changeDetector = inject(ChangeDetectorRef)
 
-  counter = 0;
-  constructor(){
-    setInterval(() => {
-      this.counter++
-      console.log("Counter: ", this.counter)
-    }, 1000 )
+  // counter = 0;
+  // constructor(){
+  //   setInterval(() => {
+  //     this.counter++
+  //     this.changeDetector.detectChanges()
+  //     console.log("Counter: ", this.counter)
+  //   }, 1000 )
 
 
-    setInterval(() => {
-      this.changeDetector.detectChanges()
-    }, 5000)
-  }
+  //   // setInterval(() => {
+  //   //   this.changeDetector.detectChanges()
+  //   // }, 1000)
+  // }
 
-  doNothing(){
+  // doNothing(){
+  //   console.log("Do Nothing....")
+  // }
 
+  // randomNumber(){
+  //   console.log("Random Number Called...")
+  //   return 100;
+  // }
+
+  counter$ = interval(1000)
+  constructor(){}
+  randomNumber(){
+    console.log("Random Number Called....")
+    return 100;
   }
 
 }
