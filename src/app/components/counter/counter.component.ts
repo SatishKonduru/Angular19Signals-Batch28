0543bb2ea37833badfce2429a23327b731526b14
satish <satishkonduru0225@gmail.com>
2025-02-11 18:00:09 +0530

    Injection context

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -3,10 +3,13 @@ import {
   Component,
   DestroyRef,
   inject,
+  Injector,
   OnDestroy,
   OnInit,
+  runInInjectionContext,
 } from '@angular/core';
 import { interval, Observable, Subscription, tap } from 'rxjs';
+import { counter } from '../../utilityFn';
 
 @Component({
   selector: 'app-counter',
@@ -14,19 +17,31 @@ import { interval, Observable, Subscription, tap } from 'rxjs';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
 })
-export class CounterComponent implements OnInit {
-  // values: Subscription;
-  values$: Observable<any>;
-  // private _destroyRef = inject(DestroyRef);
-  constructor() {}
-  ngOnInit(): void {
-    const _destroyRef = inject(DestroyRef);
-    // this.values = interval(1000).subscribe(console.log);
-    // this._destroyRef.onDestroy(() => this.values.unsubscribe());
-    this.values$ = interval(1000).pipe(tap((v) => console.log('Value: ', v)));
+export class CounterComponent {
+  // // values: Subscription;
+  // values$: Observable<any>;
+  // // private _destroyRef = inject(DestroyRef);
+  // constructor() {}
+  // ngOnInit(): void {
+  //   const _destroyRef = inject(DestroyRef);
+  //   // this.values = interval(1000).subscribe(console.log);
+  //   // this._destroyRef.onDestroy(() => this.values.unsubscribe());
+  //   this.values$ = interval(1000).pipe(tap((v) => console.log('Value: ', v)));
+  // }
+
+  // // ngOnDestroy(): void {
+  // //   this.values.unsubscribe();
+  // // }
+  // private _dr = inject(DestroyRef);
+  private _injector = inject(Injector);
+  constructor() {
+    // counter();
   }
 
-  // ngOnDestroy(): void {
-  //   this.values.unsubscribe();
-  // }
+  ngOnInit() {
+    // counter(this._dr);
+    runInInjectionContext(this._injector, () => {
+      counter();
+    });
+  }
 }
---
+++ src/app/utilityFn.ts
@@ -0,0 +1,8 @@
+import { DestroyRef, inject } from '@angular/core';
+import { interval } from 'rxjs';
+
+export function counter() {
+  const destroyRef = inject(DestroyRef);
+  const values = interval(1000).subscribe(console.log);
+  destroyRef.onDestroy(() => values.unsubscribe());
+}
